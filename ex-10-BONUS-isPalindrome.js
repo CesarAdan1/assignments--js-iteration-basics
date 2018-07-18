4ab375ef42aef5fb17640105890681747feb6a84
CesarAdan1 <whoclash@hotmail.com>
2018-07-17 20:46:23 -0500

    Hello

---
+++ ex-10-BONUS-isPalindrome.js
@@ -0,0 +1,43 @@
+
+/**
+ * isPalindrome()
+ *
+ * Write a function called `isPalindrome` that sees if the word
+ * is equal when spelled same backward as it is forward
+ *
+ * Hint: you may want to invoke the `reverseString` function you wrote earlier
+ * from the previous queestion
+*/
+
+// ++ Write YOUR CODE below
+
+function isPalindrome(string){
+    var newArray = Array.from(string);
+    var finalWord="";
+    var endStart = newArray.length - 1;
+
+    for (var i = endStart; i >= 0; i--){
+        lastWord += newArray[i];
+    }
+    if (lastWord === string){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+console.log("==== ex-10--isPalindrome : TEST 1 ====");
+
+console.assert( isPalindrome('wow') === true)
+console.assert( isPalindrome('milk') === false)
+console.assert( isPalindrome('howdy') === false)
+console.assert( isPalindrome('kayak') === true)
+console.assert( isPalindrome('tacocat') === true)
+console.assert( isPalindrome('1201411') === false)
+console.assert( isPalindrome('13100131') === true)
